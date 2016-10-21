4dab81d722895bde797959c2c9350baf920fb460
parreirinha <a39329@alunos.isel.pt>
2016-10-21 01:12:48 +0100

    layout changes

--- app/src/main/java/pdm/isel/yawa/MainActivity.kt
+++ app/src/main/java/pdm/isel/yawa/MainActivity.kt
@@ -1,11 +1,14 @@
 package pdm.isel.yawa
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
+import pdm.isel.yawa.R.id.imageView
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +16,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("YAWA_TAG", "onCreate")
+
+        //val imageView = ImageView(this)
+        //imageView.setImageResource(R.drawable.slb)
+
+
     }
 
     fun onCity(view: View){
